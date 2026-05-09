94f708d0daa9867189a584218e20b80e80bed7b0
Aryan Shah <qgm3818@aut.ac.nz>
2026-05-09 14:13:09 +1200

    Basic Document Retrieval Implementation

---
+++ retrieval_pipeline.py
@@ -0,0 +1,24 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+persistent_directory = "db/chroma_db"
+
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+db = Chroma(persist_directory=persistent_directory, embedding_function=embedding_model, collection_metadata={"hnsw:space":"cosine"})
+
+query = "What was NVIDA's first graphics accelerator called?"
+
+retriever = db.as_retriever(search_kwargs={"k": 3})
+
+# retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"k": 5, "score_threshold": 0.3}) #Only return chunks with cosine similarity score above 0.3
+
+relevant_docs = retriever.invoke(query)
+
+print(f"Urban Query: {query}\n")
+print("--- Context ---");
+for i, doc in enumerate(relevant_docs,1):
+    print(f"Document {i}: \n{doc.page_content}\n")
